432a7f2333bd6306fb9185589c4f31fd20474ab8
Linda Allen <lindaallenx@gmail.com>
2017-01-26 19:57:55 -0500

    review changes

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ function NeighborhoodMap(locations, options) {
     this.searchReq = ko.observable('');
     this.filtered = ko.observable(this.locations);
     this.geocoder = new google.maps.Geocoder();
+
+    this.modal = $('#errorModal')
 }
 
 NeighborhoodMap.prototype = Object.create({});
@@ -19,23 +21,21 @@ NeighborhoodMap.prototype.filterLocations = function (searchTerm) {
     console.log(this.locations);
     return this.locations.filter(function (location) {
         // Clear the Timers and Map Markers
-        location.marker.setMap(null);
+        location.marker.setVisible(false);
         clearTimeout(location.timer);
         // Filter by Search Term
         var titleSearch = location.title.toLowerCase().indexOf(searchTerm),
             categorySearch = location.category.toLowerCase().indexOf(searchTerm);
         return ((titleSearch > -1 || categorySearch > -1) && location.status() === 'OK');
     }, this).map(function (location, i) {
-        // delay animation
-        // location.timer = setTimeout(location.marker.setMap.bind(null, this.map), i * 250);
-        location.marker.setMap(this.map);
-        console.log(location);
+        location.marker.setVisible(true);
         return location;
     }, this);
 };
 
 NeighborhoodMap.prototype.setPosition = function (location) {
-    var map = this.map;
+    var map = this.map,
+        modal = this.modal;
     this.geocoder.geocode({'address': location.address}, function (results, status) {
         if (status === 'OK') {
             location.marker.position = results[0].geometry.location;
@@ -43,19 +43,20 @@ NeighborhoodMap.prototype.setPosition = function (location) {
             location.marker.setMap(map);
         } else if (status === 'OVER_QUERY_LIMIT') {
             // If status is OVER_QUERY_LIMIT, then wait and re-request
-            console.info("in over limit");
+            modal.find('.modal-message').text("Google Geocoder API in over rate limit");
+            modal.modal('show');
             setTimeout(this.setPosition.bind(this, location), 2000);
         } else {
-            //If status is any other error code, then set status to Error, which will remove it from list and map
             location.status('ERROR');
-            //Log error information to console
-            console.error('Error code: ', status, 'for Location:', location.title);
+            modal.find('.modal-message').text('Error code: ' + status + 'for Location:' + location.title);
+            modal.modal('show');
         }
     });
 };
 
 NeighborhoodMap.prototype.setBubble = function (location) {
-    var infowindow = this.infowindow;
+    var infowindow = this.infowindow,
+        modal = this.modal;
     google.maps.event.addListener(location.marker, 'click', function () {
 
         //Request Yelp info, then format it, and place it in infowindow
@@ -71,6 +72,9 @@ NeighborhoodMap.prototype.setBubble = function (location) {
                 "<p>" + data.snippet_text + "</p>" +
                 "</div>";
             infowindow.setContent(contentString);
+        }, function (error) {
+            modal.find('.modal-message').text(error);
+            modal.modal('show');
         });
 
         infowindow.open(this.map, location.marker);
@@ -78,28 +82,25 @@ NeighborhoodMap.prototype.setBubble = function (location) {
 };
 
 NeighborhoodMap.prototype.toggleBounce = function (location) {
-    console.info('here');
     if (location.marker.getAnimation() !== null) {
         location.marker.setAnimation(null);
     } else {
+        new google.maps.event.trigger(location.marker, 'click');
         location.marker.map.setCenter(location.marker.position);
         location.marker.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout(function () {
             location.marker.setAnimation(null);
-        }, 1900);
+        }, 700);
     }
 };
 
 NeighborhoodMap.prototype.start = function () {
-    console.info(this.locations);
     this.locations.map(function (location, i) {
         this.setPosition(location);
         this.setBubble(location);
     }, this);
-    this.searchReq.subscribe(function(searchTerm){
+    this.searchReq.subscribe(function (searchTerm) {
         var filteredResults = this.filterLocations(searchTerm);
-        console.info(filteredResults);
         this.filtered(filteredResults);
     }.bind(this));
-
 };
